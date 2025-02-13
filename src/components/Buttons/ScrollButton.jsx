45121ec07342fff913be54fdce5802687569a7b5
Daniel Lourenço Affonso <danielplayeraffonso@gmail.com>
2025-02-13 19:54:40 -0300

    AnimatedDiv criado  (ainda precisa de correcoes) :: ScrollButton modificado para maior responsividade

--- src/components/Buttons/ScrollButton.jsx
+++ src/components/Buttons/ScrollButton.jsx
@@ -16,7 +16,7 @@ const ScrollButton = (
     };
 
     return (
-        <a className="z-0 rounded-full bottom-8 transition " href={`#${href}`} onClick={handleClick}>
+        <a className={`z-0 rounded-full bottom-8 transition`} href={`#${href}`} onClick={handleClick}>
             <MdOutlineArrowCircleDown
                 className=" click:animate-ping transition  size-24 text-yellow-600 hover:bg-opacity-30 hover:bg-yellow-400 animate-bounce rounded-full bg-violet-700 bg-cover"
             />
@@ -25,6 +25,7 @@ const ScrollButton = (
 
 }
 ScrollButton.propTypes = {
-    href: PropTypes.string
+    href: PropTypes.string,
+    style: PropTypes.string
 }
 export default ScrollButton
