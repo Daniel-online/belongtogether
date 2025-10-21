e56edaa0c4457980e57d4f6ffedce22cb53778f7
daniel-online <danielplayeraffonso@gmail.com>
2025-10-20 22:29:56 -0300

    Arquivos atualizados e Config do Vite

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     // Allow the specific ngrok host
-    allowedHosts: ['https://02cb-179-100-35-100.ngrok-free.app'],
+    // allowedHosts: ['https://02cb-179-100-35-100.ngrok-free.app'],
     // Optionally, if you want to listen on all network interfaces:
     host: true,
   },
   plugins: [react()],
+  base: './', // This is important for relative paths
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
+  publicDir: 'public',
 })
