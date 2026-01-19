aea5da3fab55cb0c7dfdfaa5075a1ff8c7111a8c
daniel-online <danielplayeraffonso@gmail.com>
2026-01-19 14:57:39 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -139,12 +139,10 @@ Durante o processo, também pode ser incorporada a Lingam ou Yoni Massagem, elev
                 sectionStyle={"bg-black"}
                 sideDirection={false}
                 text={`
-                    Descubra a Massagem Relaxante elevada a uma nova arte: um encontro de profundo bem-estar e despertar sensual, guiado por **mãos profissionais** e experientes.
+                   
+Trabalha toda a musculatura, proporcionando alívio de tensões e relaxamento profundo. Técnicas de fricção, amassamento, shiatsu, reflexologia podal e alongamentos promovem o fluxo de energia pelo corpo, cuidando da sua saúde física e emocional.
 
-Nossa  técnica é a soma de anos de prática e aprimoramento, integrando o melhor das massagens clássicas com o toque sutil e consciente que evoca o erotismo e o prazer. Cada movimento é executado com precisão e sensibilidade, visando não apenas o alívio das tensões musculares, mas a expansão de todas as suas sensações.
-
-Permita-se entregar a este ritual onde o cuidado profissional se une ao despertar do erotismo. É o momento ideal para desligar a mente, relaxar profundamente e redescobrir a beleza e a sensualidade do seu próprio corpo, através de um toque que é técnico, respeitoso e inesquecível.
-
+Uma experiência essencial para restaurar o bem-estar completo e renovar suas energias.
 
 `}
 
@@ -164,9 +162,12 @@ Permita-se entregar a este ritual onde o cuidado profissional se une ao desperta
                 sectionStyle={"bg-black"}
                 sideDirection={true}
                 text={`
-Trabalha toda a musculatura, proporcionando alívio de tensões e relaxamento profundo. Técnicas de fricção, amassamento, shiatsu, reflexologia podal e alongamentos promovem o fluxo de energia pelo corpo, cuidando da sua saúde física e emocional.
+                     Descubra a Massagem Relaxante elevada a uma nova arte: um encontro de profundo bem-estar e despertar sensual, guiado por **mãos profissionais** e experientes.
 
-Uma experiência essencial para restaurar o bem-estar completo e renovar suas energias.
+Nossa  técnica é a soma de anos de prática e aprimoramento, integrando o melhor das massagens clássicas com o toque sutil e consciente que evoca o erotismo e o prazer. Cada movimento é executado com precisão e sensibilidade, visando não apenas o alívio das tensões musculares, mas a expansão de todas as suas sensações.
+
+Permita-se entregar a este ritual onde o cuidado profissional se une ao despertar do erotismo. É o momento ideal para desligar a mente, relaxar profundamente e redescobrir a beleza e a sensualidade do seu próprio corpo, através de um toque que é técnico, respeitoso e inesquecível.
+
 
 
                                         `}
